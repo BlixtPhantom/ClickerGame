046498a6a235143dec8307492054949b512dfa86
Mason Williams <williamsmason169@yahoo.com>
2024-10-27 20:30:55 -0400

    Update MainControl.gd

--- MainControl.gd
+++ MainControl.gd
@@ -12,6 +12,7 @@ func addcopper():
 	$CopperLabel
 	#This is a test for github integration between mac and windows
 	#THIS IS A TEST
+	#back and forth
 func addsilver():
 	silver += silveradd
 	print("You have ", silver, " silver")
