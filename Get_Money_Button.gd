bf9ed04c66828b9937b1175ef2e9bd56acdffd3d
BlixtPhantom <158350522+BlixtPhantom@users.noreply.github.com>
2024-10-27 20:17:35 -0400

    INIT
    Submit initial files

---
+++ Get_Money_Button.gd
@@ -0,0 +1,8 @@
+extends Button
+
+var copper = 0
+
+
+func _on_pressed():
+	copper += 1
+	print("You have ", copper, " copper")
---
+++ MainControl.gd
@@ -0,0 +1,40 @@
+extends Control
+
+var copper = 0
+var copperadd = 1
+var silver = 0
+var silveradd = 1
+
+
+func addcopper():
+	copper += copperadd
+	print("You have ", copper, " copper")
+	$CopperLabel
+
+func addsilver():
+	silver += silveradd
+	print("You have ", silver, " silver")
+
+func _ready():
+	$"Copper Button".pressed.connect(func():
+		addcopper()
+	)
+	
+	$"Silver Button".pressed.connect(func():
+		addsilver()
+	)
+	
+	$"Copper Button/Upgrade Copper".pressed.connect(func():
+		if silver > 5:
+			silver -= 5
+			print("You have spent 5 silver you now have ", silver, " silver")
+			print("You have upgraded copper generation!")
+			copperadd += 1
+		
+		else:
+			print("You do not have da funds for dat mon")
+	)
+	
+	$"Silver Button/Upgrade Silver".pressed.connect(func():
+		pass
+	)
