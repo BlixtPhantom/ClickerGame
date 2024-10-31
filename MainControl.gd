7f6aa7d10d865cc71ae4585188d454357f7d6bd8
BlixtPhantom <158350522+BlixtPhantom@users.noreply.github.com>
2024-10-31 12:26:05 -0400

    Timer function

--- MainControl.gd
+++ MainControl.gd
@@ -2,7 +2,7 @@ extends Control
 
 var phantomTokens = 0.0
 var phantomTokensAdd = .01
-var multiplier = .01
+var multiplier = 1.0
 
 
 func addPhantomTokens(multiplier):
@@ -14,6 +14,11 @@ func addPhantomTokens(multiplier):
 
 func _ready():
 	$"Phantom Tokens Button".pressed.connect(func():
+		$"Phantom Tokens Button/Multiplier Timer".start()
 		multiplier += .01
 		addPhantomTokens(multiplier)
 	)
+
+
+func _on_multiplier_timer_timeout():
+	multiplier = 1.00
